14a7e2af6e0bd0cf3ae6dde3234fafeeb2bd2792
runaround <runaroundontheground@gmail.com>
2024-09-23 10:18:35 -0600

    learning stuff Mon, Sep 23, 2024 10:18:35 AM

--- script.js
+++ script.js
@@ -1,59 +1,127 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    const emailField = document.getElementById('email');
-    const passwordField = document.getElementById('password');
-    let errors = [];
+/* // functions
 
-    // Email validation (simple regex for valid email pattern)
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!emailField.value || !emailPattern.test(emailField.value)) {
-        errors.push("Invalid email.");
+console.log(this);
+class myClass {
+    constructor() {
+        this.x = 0;
+        // "this" refers to itself as a class
     }
-
-    // Password validation (using regex defined in the pattern attribute)
-    const passwordPattern = new RegExp(passwordField.pattern);
-    if (!passwordField.value || !passwordPattern.test(passwordField.value)) {
-        errors.push("Invalid password.");
-    }
-
-    // If errors exist, prevent form submission and show alert
-    if (errors.length > 0) {
-        event.preventDefault(); // Prevent form submission
-        alert(errors.join(" ")); // Show all errors in a single alert
-    }
-});
-
-
-
-const slideshowImage = document.getElementById("slideshowImage");
-let slideshowImageIndex = 0;
-const images = [
-    "bread 1.JPG",
-    "bread 2.jpg",
-    "bread 3.jpg"
-]
-
-slideshowImage.src = images[slideshowImageIndex];
-
-function switchLeft() {
-    slideshowImageIndex -= 1;
-    if (slideshowImageIndex < 0) {
-        slideshowImageIndex = images.length - 1;
-    }
-    slideshowImage.style.opacity = 0;
-    setTimeout(() => {
-        slideshowImage.src = images[slideshowImageIndex];
-        slideshowImage.style.opacity = 1;
-    }, 250);
 }
 
-function switchRight() {
-    slideshowImageIndex += 1;
-    if (slideshowImageIndex >= images.length) {
-        slideshowImageIndex = 0;
-    }
-    slideshowImage.style.opacity = 0;
-    setTimeout(() => {
-        slideshowImage.src = images[slideshowImageIndex];
-        slideshowImage.style.opacity = 1;
-    }, 250)
+let test = new myClass();
+
+const test2 = () => {
+    this.y = 0;
+    // this uses global scope
+    return this;
 }
+
+let test3 = test2();
+
+console.log(test, test3);
+const myFunction2 = function () {
+    console.log(this);
+}
+myFunction2();
+
+const myFunction3 = () => {
+    console.log(this);
+}
+myFunction3();
+
+() => {
+    console.log("does this do anything");
+}
+
+// functions are objects
+function a() {
+    let variable = 0;
+}
+// console.log(variable); causes an error, variable is undefined
+
+
+thing();
+
+function thing() {
+    console.log("thing called using code magic");
+} */
+
+/* function colors(red="red", blue="blue", purple="purple") {
+    console.log(red, blue, purple);
+}
+
+const add = (a, b) => a + b;
+
+colors();
+let myColors = colors;
+myColors();
+
+let num = add(5, 10); */
+
+// method: function of an object
+/* 
+const clickFunction = function () {
+    console.log("stuff");
+}
+document.body.onload = function() {
+    console.log("test");
+}
+
+document.addEventListener("clicK", clickFunction);
+// document.removeEventListener("click", clickFunction); */
+
+let number1 = 0;
+let operator = null;
+let number2 = 0;
+
+const output = document.getElementById("output");
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const divide = (a, b) => {
+    if (a === 0 || b === 0) return "Divide by 0 error";
+    return a / b;
+}
+const multiply = (a, b) => a * b;
+const mod = (a, b) => a % b;
+
+function reset() {
+    number1 = 0;
+    number2 = 0;
+    operator = null;
+    output.innerText = 0;
+}
+
+function modifyOutput(modification) {
+    output.innerText += modification;
+}
+
+function nextNumber(operation) {
+    number1 = Number(output.innerText);
+    output.innerText = "0";
+    operator = operation;
+}
+
+function calculate() {
+    let result = "NaN";
+    let number2 = Number(output.innerText);
+    switch (operator) {
+        case "+":
+            result = add(number1, number2);
+            break;
+        case "-":
+            result = subtract(number1, number2);
+            break;
+        case "/":
+            result = divide(number1, number2);
+            break;
+        case "*":
+            result = multiply(number1, number2);
+            break;
+        case "%":
+            result = mod(number1, number2);
+            break;
+    }
+    output.innerText = result;
+}
+
