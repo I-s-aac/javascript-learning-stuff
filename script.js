96741dd22142d52897417b64fb0ff0a2296a90d2
runaround <runaroundontheground@gmail.com>
2024-09-30 10:18:55 -0600

    wow, i know a lot more about async stuff and promises Mon, Sep 30, 2024 10:18:55 AM

--- script.js
+++ script.js
@@ -1,74 +1,225 @@
 "use strict";
 
-// when you don't realize it's a sports analogy until you read about the goalkeeper lol
-class Player {
-    #speed;
-    #stamina;
-    constructor(name, x, y, xv, yv) {
-        this.name = name ?? "player";
-        this.x = x ?? 0;
-        this.y = y ?? 0;
-        this.xv = xv ?? 0;
-        this.yv = yv ?? 0;
-        this.position = [this.x, this.y];
-        this.velocity = [this.xv, this.yv];
-        this.skills = {
-            "jump": true,
-            "duck": false
-        }
-        this.#speed = 10;
-        this.#stamina = 100;
+// class User {
+//     constructor() {
+//         this.firstName = "";
+//         this.lastName = "";
+//         this.email = "";
+//         // don't store passwords like this
+//         this.phone = 1111111111111;
+//         this.cart = new Cart();
+//     }
+// }
+
+// class BusinessUser extends User {
+//     constructor() {
+//         super();
+
+//         this.businessName = "";
+//         this.businessAddress = "a street address";
+//         this.address2 = "another address"; // optional
+//         this.city = "";
+//         this.state = "";
+//         this.zip = "";
+//         this.employeeCount = 0;
+//         this.industry = "";
+//     }
+// }
+
+// class Product {
+//     #reviews = [];
+//     constructor() {
+//         this.price = 0.00;
+//         this.title = "title of product";
+//         this.description = "description of item";
+//         this.stars = 1; // range 1-5
+//         this.features = {
+//             "feature1": "stuff"
+//         };
+//         this.specs = {
+//             "general": {
+//                 "name": "value"
+//             },
+//             "other": {
+//                 "name": "value"
+//             }
+//         }
+//         this.images = {};
+//     }
+
+//     set addReview(review) {
+//         this.#reviews.push(review);
+//         // thing to do math with the new review's star amount
+//     }
+// }
+
+// class Cart {
+//     constructor() {
+//         this.items = {};
+//     }
+
+//     addItem(item) {
+//         this.items.push(item);
+//     }
+
+//     removeItem(item) {
+//         this.items = this.items.filter((i) => i.id !== item.id)
+//     }
+// }
+
+
+
+
+// const myPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("myPromise");
+//         Math.random() > 0.5 ? resolve() : reject();
+//     }, 1000);
+// });
+
+// const myPromise2 = () =>
+//     new Promise((resolve, reject) => {
+//         Math.random() > 0.5 ? console.log(test) : console.log("didn't cause an error");
+//         setTimeout(() => {
+//             resolve("hello");
+//         }, 300);
+//     })
+
+// myPromise2()
+//     .then((thing) => {
+//         console.log(thing, " <-- output from resolve");
+//     })
+//     .catch((error) => {
+//         console.log(error, " <-- output from reject or errors");
+//     })
+//     .finally(() => {
+//         console.log("always runs after the promise is finished");
+//     });
+
+
+// const test = param => param**2;
+// console.log(test(4));
+
+// fetch("https://swapi.dev/api/people/1/")
+//     .then((response) => {
+//         console.log(response);
+//         if (!response.ok) { throw new Error("error in fetch") }
+//         return response.json();
+//     })
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch((error) => {
+//         console.warn("error in fetch: ", error);
+//     })
+
+
+// const timeoutFunc = (delay) => {
+//     setTimeout(() => {
+//         console.log("setTimeout 1");
+//         setTimeout(() => {
+//             console.log("setTimeout 2");
+//             setTimeout(() => {
+//                 console.log("setTimeout 3");
+//                 setTimeout(() => {
+//                     console.log("setTimeout 4");
+//                 }, delay);
+//             }, delay);
+//         }, delay);
+//     }, delay);
+// };
+// // timeoutFunc(100);
+// let num = 0;
+// const promiseFunc = (delay) =>
+//     new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             num++;
+//             console.log("timeout ", num);
+//             num < 4 ? resolve() : reject();
+//         }, delay);
+//     })
+//         .then(() => {
+//             promiseFunc(delay);
+//         })
+//         .catch(() => {
+
+//         })
+
+// // promiseFunc(500);
+// let delay = 500;
+// const promiseTimeout = () => {
+//     new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("hi");
+//             resolve();
+//         }, delay);
+//     })
+// }
+
+// promiseTimeout();
+
+
+// const promiseTimeout = (delay) => {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve();
+//         }, delay)
+//     })
+// }
+
+// let delay = 500;
+// const asyncArrow = async (delay) => {
+//     try {
+//         await promiseTimeout(delay);
+//         console.log("timeout 1");
+
+//         await promiseTimeout(delay);
+//         console.log("timeout 2");
+
+//         await promiseTimeout(delay);
+//         console.log("timeout 3");
+
+//         await promiseTimeout(delay);
+//         console.log("timeout 4");
+//     } catch (error) {
+//         console.log("an error happened: ", error);
+//     }
+
+// }
+
+// asyncArrow(delay);
+
+
+const test = async (url) => {
+    try {
+        const response = await fetch("https://swapi.dev/api/people/1/");
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+
+    } catch (error) {
+        console.warn("error: ", error);
     }
+}
+// test();
 
-    get speed() {
-        return this.#speed;
-    }
+const loadButton = document.getElementById("loadButton");
+const nameDiv = document.getElementById("name");
+const eyeColorDiv = document.getElementById("eyeColor");
 
-    set speed(speed) {
-        this.#speed = typeof speed === "number" ? speed : this.#speed;
-    }
-
-    get stamina() {
-        return this.#stamina;
-    }
-
-    
-
-    set stamina(stamina) {
-        this.#stamina = typeof stamina === "number" ? stamina : this.#stamina;
-    }
-
-    displayInfo() {
-        for (const key in this) {
-            console.log(key, ": ", this[key]);
-        }
+const updateInfo = async () => {
+    try {
+        const response = await fetch("https://swapi.dev/api/people/1/");
+        const data = await response.json();
+        const name = data.name;
+        const eyeColor = data.eye_color;
+        nameDiv.innerText = name;
+        eyeColorDiv.innerText = eyeColor;
+    } catch (error) {
+        console.log("error from updateInfo", error);
     }
 }
 
-class GoalKeeper extends Player {
-    constructor(name, x, y, xv, yv) {
-        super(name, x, y, xv, yv);
-        this.saves = "none";
-    }
-    
-    makeSave() {
-        this.saves = "wow a thing";
-    }
+loadButton.onclick = (event) => {
+    updateInfo();
 }
-
-const player1 = new Player("jim");
-const player2 = new Player("bob");
-const player3 = new Player();
-const goalKeeper = new GoalKeeper("joe");
-
-player1.displayInfo();
-player2.displayInfo();
-player3.displayInfo();
-console.log(player1.speed);
-player1.speed = 100;
-console.log(player1.speed);
-
-console.log(goalKeeper.saves);
-goalKeeper.makeSave();
-console.log(goalKeeper.saves);
-goalKeeper.displayInfo();
