4ffe7cc07d04788cdd8a1b47964117e4bc78b613
runaround <runaroundontheground@gmail.com>
2024-10-04 08:29:57 -0600

    doing api stuff Fri, Oct  4, 2024  8:29:57 AM

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 "use strict";
-// challenge 1
-// [1,2,2,1]
-// find the first number that repeats (would be 2 in this case)
-// apparently can also be done using a set, but i didn't know what a set was until today
 
-// let array = [1, 2, 2, 1];
-let array = ["a", "b", "c", "b"];
+const div = document.getElementById("div");
 
-function findNthRepeat(array, count = 1) { // could be slightly faster without for...of
-    let timesSeen = {};
+async function getInfo() {
+    // could potentially use a `` string to automatically detect user location, time, etc and input that to the
+    // link to get weather info for a user's area
+    const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,apparent_temperature,precipitation&forecast_days=1");
+    const data = await response.json();
+    return data;
+}
 
-    for (const item of array) {
-        if (timesSeen[item] === count) {
-            return item;
+async function doStuff() {
+    const weatherData = await getInfo();
+
+    for (const key in weatherData.current) {
+        div.innerHTML += `
+            ${key}: 
+            ${weatherData.current[key]}
+            ${weatherData.current_units[key]}
+            <br>
+        `
+    }
+
+    for (const key in weatherData) {
+        if (key !== "current" && key !== "current_units") {
+            div.innerHTML += `
+                ${key}: 
+                ${weatherData[key]}
+                <br>
+            `
         }
-
-        timesSeen[item] = timesSeen[item] ?? 0;
-        timesSeen[item] += 1;
     }
 }
+
+
+
+doStuff();
