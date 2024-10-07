b852e7a82298e9e8472c9e44ea6661584f7c2f08
runaround <runaroundontheground@gmail.com>
2024-10-07 10:12:21 -0600

    leanred stuff Mon, Oct  7, 2024 10:12:21 AM

--- script.js
+++ script.js
@@ -1,52 +1,167 @@
 "use strict";
-
-// const div = document.getElementById("div");
-// 
-// async function getInfo() {
-//     // could potentially use a `` string to automatically detect user location, time, etc and input that to the
-//     // link to get weather info for a user's area
-//     const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,apparent_temperature,precipitation&forecast_days=1");
-//     const data = await response.json();
-//     return data;
-// }
-
-// async function doStuff() {
-//     const weatherData = await getInfo();
-
-//     for (const key in weatherData.current) {
-//         div.innerHTML += `
-//             ${key}: 
-//             ${weatherData.current[key]}
-//             ${weatherData.current_units[key]}
-//             <br>
-//         `
-//     }
-
-//     for (const key in weatherData) {
-//         if (key !== "current" && key !== "current_units") {
-//             div.innerHTML += `
-//                 ${key}: 
-//                 ${weatherData[key]}
-//                 <br>
-//             `
-//         }
-//     }
-// }
+// the stack
+// last in, first out (how you move stuff in a stack, ex. push/pop)
+// peek (return top element w/o removing), clear, and size, are other methods for doing stack stuff
+// stacks are arrays, but different bc semantic meaning and time complexity, stack have O(1) time complex methods
+// stack methods: push, pop, peek, size
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+    push(item) {
+        this.items.push(item);
+    }
+    pop() {
+        return this.isEmpty() ? null : this.items.pop();
+    }
+    peek() {
+        return this.isEmpty() ? null : this.items[this.items.length - 1];
+    }
+    size() {
+        return this.items.length;
+    }
+    isEmpty() {
+        return this.size() === 0;
+    }
+}
 
 
+let string = "{}{aa}a}{a{()aaa}";
 
-// doStuff();
+function checkThing() {
+    const stack = new Stack();
+    let extraStuff = string.match(/[^(){}[\]]/g);
+    let str = string.replace(/[^(){}]+/g, "");
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (
+            (stack.peek() === "{" && str[i] === "}") ||
+            (stack.peek() === "(" && str[i] === ")") ||
+            (stack.peek() === "[" && str[i] === "]")
+        ) {
+            stack.pop();
+        } else if (str[i] === "(" || str[i] === "{" || str[i] === "[") {
+            stack.push(str[i]);
+        }
+    }
+    if (stack.size() !== 0) {
+        console.warn("doesn't work");
+    } else {
+        console.log("works");
+    }
+    console.log(extraStuff.join(""));
+}
+checkThing();
+
+function betterThing() {
+    const stack = new Stack();
+    const open = ["{", "(", "["];
+    const close = ["}", ")", "]"];
+    const matches = {
+        "{": "}",
+        "(": ")",
+        "[": "]"
+    }
+
+    for (const char of string) {
+        if (open.includes(char)) {
+            stack.push(char);
+        } else if (close.includes(char)) {
+            if (stack.isEmpty()) {
+                return false;
+            }
+            // mismatched brackets
+            if (matches[stack.pop() !== char]) {
+                return false;
+            }
+        } else {
+            // do stuff?
+        }
+    }
+}
 
 
-// data structures
-// way to do stuff with information
+// reverse polish notation
+// num num num + *
+// num + num * num?
 
-// complexity
-// less is good
+// weak map
+// const items = new WeakMap();
 
-// do algorithms and stuff more efficiently
-// big O complexity
-// O(1) very good O()
 
-// binary search
-// repeatedly divide a thing in half to look more efficiently (think looking at page 750/1000 of a book)
+
+
+
+// queue, first in, first out
+
+
+
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+    enqueue(item) {
+        this.items.push(item);
+    }
+    dequeue() {
+        return this.isEmpty() ? null : this.items.shift();
+    }
+    peek() {
+        return this.isEmpty() ? null : this.items[0];
+    }
+    size() {
+        return this.items.length;
+    }
+    isEmpty() {
+        return this.size() === 0;
+    }
+}
+
+
+// do reverse polish notation
+// 3 4 -
+// is
+// 3 - 4
+
+// wow RPN is kinda confusing
+// but it also makes sense if you thonk hard enough
+
+function reversePolishNotation(input) {
+    const tokens = input.split(" ");
+    const stack = new Stack();
+    for (const token of tokens) {
+        if (!["+", "-", "*", "/", "%"].includes(token)) {
+            stack.push(Number(token));
+        } else {
+            let num2 = stack.pop();
+            let num1 = stack.pop();
+
+            let result = null;
+            switch (token) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                case "%":
+                    result = num1 % num2;
+                    break;
+            }
+            stack.push(result);
+        }
+    }
+
+    const finalResult = stack.pop();
+    return finalResult;
+}
+console.log(
+    reversePolishNotation("6 8 + 10 + 12 14 + -")
+);
